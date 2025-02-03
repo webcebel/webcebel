5e865be4356f3721efb91f200dd9235779a146e8
CETEO <contacto.ceteo@gmail.com>
2025-02-02 21:57:12 -0500

    arreglos responsivos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import '../styles/components/header.css';
 
 const Header = () => {
@@ -20,29 +21,52 @@ const Header = () => {
           />
         </Link>
         
-        <button className="menu-toggle" onClick={toggleMenu}>
-          <span className="hamburger"></span>
+        <button 
+          className="menu-toggle" 
+          onClick={toggleMenu}
+          aria-label="Toggle menu"
+        >
+          {isMenuOpen ? <FaTimes /> : <FaBars />}
         </button>
 
         <nav className={`navLinks ${isMenuOpen ? 'open' : ''}`}>
-          <Link to="/quienes-somos" className="nav-item">
+          <Link 
+            to="/quienes-somos" 
+            className="nav-item"
+            onClick={() => setIsMenuOpen(false)}
+          >
             ¿Quiénes somos?
           </Link>
-          <Link to="/revista" className="nav-item">
+          <Link 
+            to="/revista" 
+            className="nav-item"
+            onClick={() => setIsMenuOpen(false)}
+          >
             Revista CEBEL
           </Link>
-          <Link to="/subcomisiones" className="nav-item">
+          <Link 
+            to="/subcomisiones" 
+            className="nav-item"
+            onClick={() => setIsMenuOpen(false)}
+          >
             Subcomisiones
           </Link>
-          <Link to="/onboarding" className="nav-item">
+          <Link 
+            to="/onboarding" 
+            className="nav-item"
+            onClick={() => setIsMenuOpen(false)}
+          >
             Onboarding
           </Link>
-          {/*<Link to="/material-educativo" className="nav-item">
+          {/*<Link 
+            to="/material-educativo" 
+            className="nav-item"
+            onClick={() => setIsMenuOpen(false)}
+          >
             Material Educativo
           </Link>*/}
           <a 
-            href="https://docs.google.com/forms/d/e/1FAIpQLScOapwQ8AhbfbJxWz6sHQs5poS9ByM2oZ_eTLjpwQ36oNsVzA/viewform?usp=sharing" 
-            className="volunteer-button"
+            href="https://docs.google.com/forms/d/e/1FAIpQLScOapwQ8AhbfbJxWz6sHQs5poS9ByM2oZ_eTLjpwQ36oNsVzA/viewform?usp=sharing"             className="volunteer-button"
             target="_blank"
             rel="noopener noreferrer"
           >
